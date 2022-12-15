642bf0cfc5cdcc4bd47420e3c80a191689c9d2a2
NunezAN <alex101nunez@gmail.com>
2022-12-15 10:27:49 -0600

    completed app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from "next";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import axios from "axios";
+
 const Home: NextPage = () => {
+  type dataMuse = {
+    word: string;
+    score: number;
+  };
   let searchInput = useRef<HTMLInputElement>(null);
-  const getSearch = (event: React.FormEvent<HTMLFormElement>) => {
+  const [searchResults, setSearchResults] = useState<dataMuse[]>([]);
+  const getSearch = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const results = await axios.get(
+      `https://api.datamuse.com/words?ml=${searchInput.current?.value}`
+    );
+    setSearchResults(results.data);
     console.log("Searching...", searchInput.current?.value);
   };
   return (
-    <div className="flex min-h-screen min-w-max">
+    <div className="flex min-h-screen min-w-max flex-col">
       <form
         className="p-4 bg-slate-600 h-fit w-[80%] mx-auto my-12 rounded-2xl flex flex-col items-center space-y-4"
         onSubmit={(e) => getSearch(e)}
@@ -25,6 +37,16 @@ const Home: NextPage = () => {
           Submit
         </button>
       </form>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-2 w-[80%] grid-cols-2 mx-auto">
+        {searchResults &&
+          searchResults.map((result) => (
+            <div className="p-2">
+              <span className="uppercase text-gray-600 text-2xl py">
+                {result.word}
+              </span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
